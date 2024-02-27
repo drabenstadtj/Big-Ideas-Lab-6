ade91f38b102f889fc60d949679abd82705a29c0
drabenstadtj <jackdrabenstadt@gmail.com>
2024-02-27 14:45:36 -0500

    Handled input

---
+++ something.py
@@ -0,0 +1,8 @@
+# Take name input
+name = input("Enter a name: ")
+
+# Take other input
+other_input = input("Enter something else: ")
+
+# Ensure inputs are of the same length
+length = min(len(name), len(other_input))
