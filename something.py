4f843c9395e24f005bc6f71d4f439a0008779ceb
Aidan <aconroy412@gmail.com>
2024-02-27 14:55:15 -0500

    Wrote interpolation code

--- something.py
+++ something.py
@@ -6,3 +6,19 @@ other_input = input("Enter something else: ")
 
 # Ensure inputs are of the same length
 length = min(len(name), len(other_input))
+
+# Initialize the result string
+result = ''
+
+# Alternate letters from name and other_input
+for i in range(length):
+    result += name[i] + other_input[i]
+
+# Add remaining letters from the longer input
+if len(name) > length:
+    result += name[length:]
+elif len(other_input) > length:
+    result += other_input[length:]
+
+# Print the interpolated string
+print("Interpolated string with alternating letters:", result)
